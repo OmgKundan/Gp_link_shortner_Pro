c9e407da128fdc258be3270a0784017ec1f027cb
OmgKundan <134349990+OmgKundan@users.noreply.github.com>
2023-05-23 22:47:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
-ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+API_ID = int(os.getenv("API_ID", "23501167"))
+API_HASH = os.environ.get("API_HASH", "72f7270a1b4bd3ee93629a396ebb5004")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6093098314:AAFLevnUGEg0boQ3hrkVUSA1M9FdWK3dCM8")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5143024716")] if os.environ.get("ADMINS") else []
+ADMIN = ADMIN
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "TelegramBots")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://TelegramBot:kundanv1006x@telegrambot.phzuxsj.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5143024716")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(Id Owned Id)
 #  Optionnal variables
